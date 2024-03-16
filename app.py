ebe5a510d447e294f30b7e38836e20a8c41c8473
AbhishekYawalkar21 <abhishekyawalkar4@gmail.com>
2024-03-16 18:59:38 -0400

    changed image dimension to 224x224

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def predict():
     image_data = BytesIO(image_file.read())
 
     # Load and preprocess the image
-    img = image.load_img(image_data, target_size=(256, 256))
+    img = image.load_img(image_data, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
     img_preprocessed = img_array_expanded_dims / 255.0
